6c6627421c53b40a537defed1ed71546f958c62c
NatalieR89 <natalieritch89@gmail.com>
2024-08-11 22:26:03 +0000

    8bt

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_postorder - is a type of tree traversal
+ *@tree: the tree we're calling
+ *@func:a func call for the tree
+ *Return: nothing
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
