641af8bd4cb45484ea415be9a07d6f549ec02937
NatalieR89 <natalieritch89@gmail.com>
2024-08-11 00:06:39 +0000

    whoops

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_root - if the node is the root
+ *@node: this is the node
+ *Return: return a whole lot
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
+}
