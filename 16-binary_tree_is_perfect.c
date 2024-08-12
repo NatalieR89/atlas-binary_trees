069d6d205d5bb32ba446714a5ba38e274155ade8
NatalieR89 <natalieritch89@gmail.com>
2024-08-12 16:30:13 +0000

    fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,7 +40,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->l == NULL || tree->r == NULL)
 		return (0);
 	l = binary_tree_is_perfect(tree->l);
-	r = binary_tree_is_perfect(tree->r);
+	right = binary_tree_is_perfect(tree->r);
 	if (l == 0 || r == 0)
 		return (0);
 	if (binary_tree_height(tree->l) == binary_tree_height(tree->r))
